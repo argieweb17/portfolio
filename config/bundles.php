bc954e9e84d92c1132a35b5723ebc3af790509e0
argieweb17 <argiepairpagbunocan@gmail.com>
2026-04-12 04:02:05 +0800

    port1

--- config/bundles.php
+++ config/bundles.php
@@ -1,17 +1,31 @@
 <?php
 
-return [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
+
+$debugBundle = 'Symfony\\Bundle\\DebugBundle\\DebugBundle';
+if (class_exists($debugBundle)) {
+    $bundles[$debugBundle] = ['dev' => true];
+}
+
+$webProfilerBundle = 'Symfony\\Bundle\\WebProfilerBundle\\WebProfilerBundle';
+if (class_exists($webProfilerBundle)) {
+    $bundles[$webProfilerBundle] = ['dev' => true, 'test' => true];
+}
+
+$makerBundle = 'Symfony\\Bundle\\MakerBundle\\MakerBundle';
+if (class_exists($makerBundle)) {
+    $bundles[$makerBundle] = ['dev' => true];
+}
+
+return $bundles;
--- public/index.php
+++ public/index.php
@@ -5,5 +5,13 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return static function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $env = $context['APP_ENV'] ?? 'prod';
+    $debug = (bool) ($context['APP_DEBUG'] ?? false);
+
+    if ('dev' === $env && !class_exists('Symfony\\Bundle\\DebugBundle\\DebugBundle')) {
+        $env = 'prod';
+        $debug = false;
+    }
+
+    return new Kernel($env, $debug);
 };
